191e66c851cc7c55d68fe206371e60d4c1c77103
jwebmaster <justin@jwebmaster.be>
2023-03-09 12:43:50 +0100

    corrction

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,19 +7,51 @@ console.log(gsap.version);
 //   {opacity:1, scale:1, y:0, ease: "back.out(1.7)", duration:1, stagger: .2}
 // );
 
-gsap.from(
-  ".title--line",
-  {opacity:0, scale:.5, y:'-=400', onComplete: anim, stagger: .2}
-);
+// gsap.from(
+//   ".title--line",
+//   {opacity:0, scale:.5, y:'-=400', onComplete: anim, stagger: .2}
+// );
 
-function anim(){
-  gsap.to(
-    ".title--line",
-    {opacity:1, scale:1, y:0, ease: "back.out(1.7)", duration:1}
-  )
-};
+// function anim(){
+//   gsap.to(
+//     ".title--line",
+//     {opacity:1, scale:1, y:0, ease: "back.out(1.7)", duration:1, onComplete: animBack}
+//   )
+// };
+
+// function animBack(){
+//   gsap.fromTo(
+//     ".title",
+//     {opacity:1, scale:1, y:0, display:block},
+//     {opacity:0, scale:0, y:'+=100vh', display:none, ease: "back.out(1.7)", duration:1}
+//   )
+// };
 
 // gsap.to(
 //   ".title--line",
 //   {opacity:1, scale:1, y:0, ease: "back.out(1.7)", duration:2, stagger: .6}
 // );
+
+//Correction
+gsap.from(
+  ".title--line",
+  {
+    opacity:0,
+    duration:1,
+    y:'-=20',
+    scale:.5,
+    stagger:.5,
+    ease: "back.out(1.7)",
+    onComplete: function(){
+      gsap.to(
+        '.title',
+        {
+          duration:1,
+          opacity:0,
+          scale:0,
+          delay:2
+        }
+      )
+    }
+  }
+);
