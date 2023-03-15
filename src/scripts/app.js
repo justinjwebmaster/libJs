51b9bd4dc6d1eb77b06f97be09e4bc609273ab2c
jwebmaster <justin@jwebmaster.be>
2023-03-15 16:06:15 +0100

    Test du plugin ScrollTrigger

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,69 +1,18 @@
-import { gsap } from 'gsap';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
-console.log(gsap.version);
-// gsap.fromTo(
-//   ".title--line",
-//   {opacity:0, scale:.5, y:'-=400'},
-//   {opacity:1, scale:1, y:0, ease: "back.out(1.7)", duration:1, stagger: .2}
-// );
+gsap.registerPlugin(ScrollTrigger);
 
-// gsap.from(
-//   ".title--line",
-//   {opacity:0, scale:.5, y:'-=400', onComplete: anim, stagger: .2}
-// );
-
-// function anim(){
-//   gsap.to(
-//     ".title--line",
-//     {opacity:1, scale:1, y:0, ease: "back.out(1.7)", duration:1, onComplete: animBack}
-//   )
-// };
-
-// function animBack(){
-//   gsap.fromTo(
-//     ".title",
-//     {opacity:1, scale:1, y:0, display:block},
-//     {opacity:0, scale:0, y:'+=100vh', display:none, ease: "back.out(1.7)", duration:1}
-//   )
-// };
-
-// gsap.to(
-//   ".title--line",
-//   {opacity:1, scale:1, y:0, ease: "back.out(1.7)", duration:2, stagger: .6}
-// );
-
-
-
-//Correction
-// const anim = gsap.from(
-//   ".title--line",
-//   {
-//     paused: true,
-//     opacity:0,
-//     duration:1,
-//     y:'-=20',
-//     scale:.5,
-//     stagger:.5,
-//     ease: "back.out(1.7)",
-//     onComplete: function(){
-//       gsap.to(
-//         '.title',
-//         {
-//           duration:1,
-//           opacity:0,
-//           scale:0,
-//           delay:2
-//         }
-//       );
-//     }
-//   }
-// );
-
-// const btn = document.querySelector('.btn');
-// btn.addEventListener('click', playAnimation());
-
-// function playAnimation(){
-//   anim.timeScale(.1);
-//   anim.play();
-// }
-
+gsap.to(".box", {
+  x: 500,
+  duration: 1,
+  // scrollTrigger est un object ce qui permet
+  // d'ajouter des configurations
+  scrollTrigger: {
+    trigger: ".box",
+    start: "20px 80%",
+    end: "+=300",
+    scrub: 0.5,
+    pin: true,
+  },
+});
