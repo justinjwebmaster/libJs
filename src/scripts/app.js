425ea2af8a21b42fefe84305f3109a31c73b67a3
jwebmaster <justin@jwebmaster.be>
2023-03-15 16:20:41 +0100

    Avec du noise en fond et le fond du logo transparent

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,7 +4,7 @@ const tl = gsap.timeline();
 
 tl.to('.title', { opacity: 1, duration: 2 })
   .to('.title', { opacity: 0, duration: 2 })
-  .set('.logo', { opacity: 1, scale: 5 }, "+=1")
+  .set('.logo', { opacity: 1, scale: 3 }, "+=1")
   .to('.logo', { scale: 0, duration: 8 })
   .to('.logo', { opacity: 0, duration: 4 }, "-=4")
   .from('.def', { top: "100vh", duration: 8 }, "<");
